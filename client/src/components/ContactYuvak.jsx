9cf3a7b1ac7615bb74ae423e1549fecce0ec8c7c
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-06-20 19:57:50 +0530

    contact yuvak updated

--- client/src/components/ContactYuvak.jsx
+++ client/src/components/ContactYuvak.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export const Contact = ({post}) => {
-
+    const {currentUser} = useSelector((state)=>state.user) 
     const [landLord,setLandlord] = useState(null);
     const [message,setMessage] = useState("");
     const onhandleChange = (e)=>{
@@ -26,13 +27,19 @@ export const Contact = ({post}) => {
 
   return (
     <>
-        {landLord && (
-            <div className='flex flex-col gap-2' >
-                <p>Contact <span className='font-semibold' >{landLord.username}</span> for <span className='font-semibold' >{post.name.toLowerCase()}</span></p>
-                <textarea className='w-full border p-3 rounded-lg' name="message" id="message" rows="2" value={message} onChange={onhandleChange} placeholder='Enter your message Here' ></textarea>
-                <Link to={`mailto:${landLord.email}?subject=Regarding ${post.name}&body=${message}`} className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95' >Send Message</Link>
-            </div>
-        )}
+    {
+        currentUser.yuvakId !== post._id ?(
+            landLord && (
+                <div className='flex flex-col gap-2' >
+                    <p>Contact <span className='font-semibold' >{landLord.username}</span> for <span className='font-semibold' >{post.name.toLowerCase()}</span></p>
+                    <textarea className='w-full border p-3 rounded-lg' name="message" id="message" rows="2" value={message} onChange={onhandleChange} placeholder='Enter your message Here' ></textarea>
+                    <Link to={`mailto:${landLord.email}?subject=Regarding ${post.name}&body=${message}`} className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95' >Send Message</Link>
+                </div>
+            )
+        )
+        :""
+    }
+        
     </>
   )
 }
