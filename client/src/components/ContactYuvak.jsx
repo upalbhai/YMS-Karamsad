a8bb85e8ee6da281f99b8953865b672411d935a1
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-06-20 20:03:41 +0530

    contact us text box updated

--- client/src/components/ContactYuvak.jsx
+++ client/src/components/ContactYuvak.jsx
@@ -32,7 +32,7 @@ export const Contact = ({post}) => {
             landLord && (
                 <div className='flex flex-col gap-2' >
                     <p>Contact <span className='font-semibold' >{landLord.username}</span> for <span className='font-semibold' >{post.name.toLowerCase()}</span></p>
-                    <textarea className='w-full border p-3 rounded-lg' name="message" id="message" rows="2" value={message} onChange={onhandleChange} placeholder='Enter your message Here' ></textarea>
+                    <textarea className='w-full text-black border p-3 rounded-lg' name="message" id="message" rows="2" value={message} onChange={onhandleChange} placeholder='Enter your message Here' ></textarea>
                     <Link to={`mailto:${landLord.email}?subject=Regarding ${post.name}&body=${message}`} className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95' >Send Message</Link>
                 </div>
             )
